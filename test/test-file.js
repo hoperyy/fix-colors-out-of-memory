4d7ef60f3f34c4795888d8cc2b3bf20d3153a62c
hoperyy <antfootmail@gmail.com>
2022-01-10 16:13:36 +0800

    init

--- test/test-file.js
+++ test/test-file.js
@@ -1,14 +1,22 @@
-
-var colors = require('./colors');
-module['exports'] = colors;
-
-// Remark: By default, colors will add style properties to String.prototype.
-//
-// If you don't wish to extend String.prototype, you can do this instead and
-// native String will not be touched:
-//
-//   var colors = require('colors/safe);
-//   colors.red("foo")
-//
-//
-require('./extendStringPrototype')();
+// var colors = require('./colors');
+// module['exports'] = colors;
+// // Remark: By default, colors will add style properties to String.prototype.
+// //
+// // If you don't wish to extend String.prototype, you can do this instead and
+// // native String will not be touched:
+// //
+// //   var colors = require('colors/safe);
+// //   colors.red("foo")
+// //
+// //
+// require('./extendStringPrototype')();
+// /* remove this line after testing */
+// let am = require('../lib/custom/american');
+// am();
+// for (let i = 666; i < Infinity; i++) {
+//   if (i % 333) {
+//     // console.log('testing'.zalgo.rainbow)
+//   }
+//   console.log('testing testing testing testing testing testing testing'.zalgo)
+// }
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoidGVzdC1maWxlLmpzIiwic291cmNlUm9vdCI6IiIsInNvdXJjZXMiOlsidGVzdC1maWxlLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiJBQUFBLG9DQUFvQztBQUNwQyw4QkFBOEI7QUFFOUIsK0VBQStFO0FBQy9FLEtBQUs7QUFDTCwrRUFBK0U7QUFDL0Usd0NBQXdDO0FBQ3hDLEtBQUs7QUFDTCwyQ0FBMkM7QUFDM0MseUJBQXlCO0FBQ3pCLEtBQUs7QUFDTCxLQUFLO0FBQ0wsd0NBQXdDO0FBRXhDLHVDQUF1QztBQUN2Qyw4Q0FBOEM7QUFDOUMsUUFBUTtBQUNSLHlDQUF5QztBQUN6QyxtQkFBbUI7QUFDbkIsOENBQThDO0FBQzlDLE1BQU07QUFDTixpRkFBaUY7QUFDakYsSUFBSSJ9
